7bcc7480b1962cf3a9960a6d10b34925011664b3
khayreddine <khayreddine.akremi@gmail.com>
2020-12-25 12:37:37 +0100

    some changes

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,12 +6,19 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDialogModule } from '@angular/material/dialog';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FeatureDetailsComponent } from './feature-details/feature-details.component';
+import { MatIconModule } from '@angular/material/icon';
 
-const MATERIAL = [MatFormFieldModule, MatInputModule, MatDialogModule];
+const MATERIAL = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatDialogModule,
+  MatIconModule,
+];
 const FORMS = [FormsModule, ReactiveFormsModule];
 
 @NgModule({
-  declarations: [FeatureFormComponent],
+  declarations: [FeatureFormComponent, FeatureDetailsComponent],
   imports: [CommonModule, ...MATERIAL, ...FORMS],
   exports: [...MATERIAL, ...FORMS],
 })
